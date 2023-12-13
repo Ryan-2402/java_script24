bc795573c2deee369c636cbbde232beb0faa8419
Ryan-2402 <153626060+Ryan-2402@users.noreply.github.com>
2023-12-13 07:03:39 +0000

    converion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "ryan"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+
+// 1 => true, 0 => false
+// "" => false
+// "ryan" => true
+
+let someNumber = 88
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
