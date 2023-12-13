2f7a890008bf032082134cdaec4b947227681207
Ryan-2402 <153626060+Ryan-2402@users.noreply.github.com>
2023-12-13 06:46:45 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" //treat all js code as newer version
+
+// alert('ryan') //we are using nodejs, not browser
+
+console.log(3 
+    + 3) 
+
+console.log("ryan");
+
+
+let name = "ryan"
+let age = 24
+let isLoggedIn = true
+let state;
+
+
+// number =>2 to power 53
+//bigInt 
+
+//string => ""
+//boolean => true/false
+//null => standAlone value
+//undefined => not define
+//symbol => unique
+
+
+//object
+
+console.log(typeof null);   // object
+console.log(typeof undefined); //undefined
