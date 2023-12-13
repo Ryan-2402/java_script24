9b8025071cefb4a84252957c725d41eaad5c9307
Ryan-2402 <153626060+Ryan-2402@users.noreply.github.com>
2023-12-13 09:55:28 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,40 @@
+const name = "ryan"
+const repocount = 40
+// console.log(name + repocount + " value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+// console.log(typeof(name));
+const gameName = new String('ryansyed-sr')
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+// // console.log(typeof(gameName));
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('n'));
+
+const newstring = gameName.substring(0, 7)
+// console.log(newstring);
+
+const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newstr1 = "    ryan    "
+console.log(newstr1);
+console.log(newstr1.trim());
+
+
+const url = "https://rysn.com/syed%10ryan"
+console.log(url.replace('%10', '-'));
+
+const newstr2 = "syed riyan syed riyan"
+console.log(newstr2.split('y'));
+
+
+
+
+
+
+
